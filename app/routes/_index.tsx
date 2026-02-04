74f6653d04daf383474073966214c4cd5d9ebdc2
rudradave633-commits <rudradave633@gmail.com>
2026-02-04 08:33:54 +0530

    fix root route and homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,29 @@
-import { redirect } from "@remix-run/cloudflare";
+import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/About";
+import RingCarousel from "~/components/RingCarousel";
+import Pricing from "~/components/Pricing";
+import Booking from "~/components/Booking";
+import GTAOnlinePolicy from "~/components/GTAOnlinePolicy";
+import GameGuidelines from "~/components/GameGuidelines";
+import GameLibraryHome from "~/components/GameLibraryHome";
+import Experience from "~/components/Experience";
+import EsportsTicker from "~/components/EsportsTicker";
+import Footer from "~/components/Footer";
 
-export async function loader() {
-  return redirect("/home");
-}
-
-export default function Index() {
-  return null;
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <EsportsTicker />
+      <AboutSection />
+      <RingCarousel />
+      <Pricing />
+      <Booking />
+      <GTAOnlinePolicy />
+      <GameGuidelines />
+      <GameLibraryHome />
+      <Experience />
+      <Footer />
+    </>
+  );
 }
