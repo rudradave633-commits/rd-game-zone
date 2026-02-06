649c5e808dce96774a234ff40920c089e70a1d73
rudradave633-commits <rudradave633@gmail.com>
2026-02-06 19:30:45 +0530

    Final stable Cloudflare Pages Remix SSR setup

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,7 +1,7 @@
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 
 // @ts-ignore - generated at build time
-import * as build from "../build/server/index.js";
+import * as build from "../build/index.js";
 
 export const onRequest = createPagesFunctionHandler({
   build,
