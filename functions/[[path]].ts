5946ab5d764fcd3c7fe93b5d675120b408ec651d
rudradave633-commits <rudradave633@gmail.com>
2026-02-06 19:39:35 +0530

    Fix Cloudflare Pages Remix server build import

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,7 +1,7 @@
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 
-// @ts-ignore - generated at build time
-import * as build from "../build/index.js";
+// @ts-ignore – generated at build time
+import * as build from "../build/server.js";
 
 export const onRequest = createPagesFunctionHandler({
   build,
